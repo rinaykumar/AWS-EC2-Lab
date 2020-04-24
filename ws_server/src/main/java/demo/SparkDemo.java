1a0fb9f1f772ae68dc8a94943db18bb6a5809e10
rinaykumar <rinaykumar@gmail.com>
2020-04-23 20:10:51 -0700

    EC2 lab backend

--- ws_server/src/main/java/demo/SparkDemo.java
+++ ws_server/src/main/java/demo/SparkDemo.java
@@ -5,6 +5,6 @@ import static spark.Spark.*;
 public class SparkDemo {
   public static void main(String[] args) {
     port(3001);
-    get("/api/hello", (req, res) -> "hi"); // test
+    get("/api/hello", (req, res) -> "Hello from Rinay Kumar"); // test
   }
 }
